8ae370ecd3cab12c7c138e6ebe0d2050d26025d1
Sjuul Janssen <sjuul@outlook.com>
2021-04-13 07:04:05 +0200

    added some docs

--- evicted-keys-variables.tf
+++ evicted-keys-variables.tf
@@ -33,7 +33,12 @@ variable "evicted_keys_note" {
 
 variable "evicted_keys_docs" {
   type    = string
-  default = ""
+  default = <<EOF
+Redis supports various eviction policies that are used by the server when memory usage hits the max limit. A persistent positive value of this metric is an indication that you need to scale the memory up.
+
+Redis supports TTL (time to live) property for each key. The server deletes the key if the associated TTL has elapsed. If the application does not define this property, it causes expired data to pile up in memory. A positive metric value shows that your expired data is being cleaned up properly.
+https://scalegrid.io/blog/6-crucial-redis-monitoring-metrics/
+EOF
 }
 
 variable "evicted_keys_filter_override" {
